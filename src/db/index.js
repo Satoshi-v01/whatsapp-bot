6688bf58eb043d3fc6a1a7237d3d80a3829cafa5
Lucas <finnfloyd@gmail.com>
2026-03-16 15:19:01 -0300

    app.js y testeo db

---
+++ src/db/index.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+const { Pool } = require('pg')
+
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL
+})
+
+pool.on('connect', () => {
+    console.log('Conectado a PostgreSQL')
+})
+
+pool.on('error', (err) => {
+    console.error('Error en la conexión a PostgreSQL:', err)
+})
+
+module.exports = pool
